a16feef9b34eec1b6c4b6109b6a352c25614d76f
Arijeet Singh <sssarijeet@gmail.com>
2026-03-08 09:56:56 +0530

    Create Airthmetic.py with arithmetic examples
    Added basic arithmetic and comparison operations in Airthmetic.py.

---
+++ Airthmetic.py
@@ -0,0 +1,49 @@
+num = 3
+
+#Airthmetic Operators;
+#Addition : 3 + 2
+#Subtraction : 3-2
+#Multiplication : 3*2
+#Divison : 3/2
+#Floor Division : 3//2
+#Exponent : 3**2
+#Modulus : 3 % 
+
+#comparison Operators;
+#equal : 3==2
+#not equal : 3!=2
+#Greater than : 3>2
+#less than : 3<2
+#Greater than equal to : 3>=2
+#less or equal : 3<=2
+
+
+
+print(2%2)
+print(3%2)
+print(3*2)
+print(3-2)
+print(3*(2+1))
+
+num = num + 1
+
+print(num)
+
+#absolute value
+print(abs(-3))
+
+#round
+print(round(3.75))
+
+
+num_1 = 3
+num_2 = 2
+print(num_1 > num_2)
+
+
+num_1 = '100'
+num_2 = '200'
+
+print(num_1 + num_2)
+
+
