cab47d29ccb061d14cc2ca7e0d623758b405d835
Arijeet Singh <sssarijeet@gmail.com>
2026-03-08 09:44:53 +0530

    Add string manipulation examples in string.py
    Add various string manipulation examples including slicing, case conversion, counting, finding, replacing, and concatenation.

---
+++ string.py
@@ -0,0 +1,33 @@
+message = 'hello World'
+
+#slicing
+print(message[6:])
+
+#toLowercase
+print(message.lower())
+
+#toUppercase
+print(message.upper())
+
+#count
+print(message.count('hello'))
+print(message.count('h'))
+
+
+#find
+print(message.find('llo'))
+
+#replace
+new_message = message.replace('World','Universe')
+print(new_message)
+
+
+#adding two string
+greeting = 'hello'
+name = 'micheal'
+message = greeting + " " + name
+print(message)
+
+
+
+
